795860681555c9ee2e5a0f5d22c87a4cb1f12e50
valdadyne <rodgers.gitau@gmail.com>
2018-03-16 23:45:33 +0300

    model for anime

---
+++ app/anime/anime.js
@@ -0,0 +1,12 @@
+"use strict";
+
+class Anime {
+  constructor(id, name, creator, genre) {
+    this.id = id;
+    this.name = name;
+    this.creator = creator;
+    this.genre = genre;
+  }
+}
+
+module.exports = Anime;
