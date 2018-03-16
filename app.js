c3e3e1958065103d3714878d65f19e5e542561c1
valdadyne <rodgers.gitau@gmail.com>
2018-03-16 23:46:37 +0300

    - added body-parser - required anime routes

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 
 const express = require("express");
 const app = new express();
+const bodyParser = require("body-parser");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// register JSON parser middlewear
+app.use(bodyParser.json());
+
+require("./app/anime/animeRoutes")(app);
 
 app.listen(3000, () => {
   console.log("Server up!");
