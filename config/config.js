b9e710987fea3f0126471119e85240adc855cdf5
valdadyne <rodgers.gitau@gmail.com>
2018-03-17 21:12:03 +0300

    - require firebase - require config file

--- config/config.js
+++ config/config.js
@@ -1,18 +1,23 @@
+const firebase = require("firebase");
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase.json");
+const { config, serviceAccount } = require("./firebase.js");
 
 // firebase init
+firebase.initializeApp(config);
+
+// firebase-admin init
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 // firebase auth
-const auth = admin.auth();
+const auth = firebase.auth();
 
 // firestore
 const db = admin.firestore();
 
 module.exports = {
+  admin: admin,
   auth: auth,
   db: db
 };
