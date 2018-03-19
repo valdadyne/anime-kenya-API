1a4d4c44b10467cbdee94ad1510c391dae80ef93
valdadyne <rodgers.gitau@gmail.com>
2018-03-19 11:20:40 +0300

    - added signout - fixed error messages alerts - enabled searching of all users

--- app/user/userController.js
+++ app/user/userController.js
@@ -1,32 +1,74 @@
 "use strict";
 
 const User = require("./user");
-const { auth } = require("../../config/config");
+const { admin, auth } = require("../../config/config");
 
 class UserController {
   constructor() {
-    this.authError = "";
     this.activeUser;
+    this.authStatus;
+    this.allUsers = [];
   }
 
   signUp(email, password) {
-    auth.createUserWithEmailAndPassword(email, password).catch(error => {
-      if (error) {
-        this.authError = error.message;
-        return this.authError;
-      }
-    });
-    return (this.authError = "");
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(userRecord => {
+        this.activeUser = userRecord.email;
+        this.authStatus = "Successful SignUp as " + this.activeUser;
+      })
+      .catch(err => {
+        this.authStatus = err.message;
+      });
+    return this.authStatus;
   }
 
   signIn(email, password) {
-    auth.signInWithEmailAndPassword(email, password).catch(error => {
-      if (error) {
-        this.authError = error.message;
-        return this.authError;
-      }
-    });
-    return (this.authError = "");
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(userRecord => {
+        this.activeUser = userRecord.email;
+        this.authStatus = "Successful SignIn as " + this.activeUser;
+      })
+      .catch(err => {
+        this.authStatus = err.message;
+      });
+    return this.authStatus;
+  }
+
+  signOut() {
+    auth
+      .signOut()
+      .then(() => {
+        this.authStatus = "Successful SignOut " + this.activeUser;
+      })
+      .catch(err => {
+        this.authStatus = err.message;
+      });
+    return this.authStatus;
+  }
+
+  getAll() {
+    admin
+      .auth()
+      .listUsers()
+      .then(listUsersResult => {
+        listUsersResult.users.forEach(userRecord => {
+          let duplicate = this.allUsers.indexOf(userRecord.email);
+          if (duplicate == -1) {
+            this.allUsers.push(userRecord.email);
+          }
+          this.authStatus = "";
+        });
+      })
+      .catch(err => {
+        this.authStatus = err;
+      });
+    if (this.authStatus === "") {
+      return this.allUsers;
+    } else {
+      return this.authStatus;
+    }
   }
 }
 
